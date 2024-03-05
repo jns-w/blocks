f02883502ee2f3ff3971eda75f5196892dd7c318
jns-w <jns.w@icloud.com>
2024-03-05 22:50:01 +0800

    prod impl

--- blocks-cli/next.config.js
+++ blocks-cli/next.config.js
@@ -1,12 +1,3 @@
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([], {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://blocks-server.capybara.wldspace.com/api/:path*'
-            }
-        ]
-    }
-});
+module.exports = withPlugins([], {});
