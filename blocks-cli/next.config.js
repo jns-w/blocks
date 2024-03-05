ca5cb622194ba9a6cb3c8b046c7b14c478cd2d6f
jns-w <jns.w@icloud.com>
2024-03-05 22:05:03 +0800

    prod impl

--- blocks-cli/next.config.js
+++ blocks-cli/next.config.js
@@ -1,3 +1,12 @@
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([], {});
+module.exports = withPlugins([], {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/:path*`
+            }
+        ]
+    }
+});
