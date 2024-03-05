1988e24dd47f70be11b5bd5720e17d91fda37c0b
jns-w <jns.w@icloud.com>
2024-03-05 22:17:24 +0800

    prod impl

--- blocks-server/connection/blocks.js
+++ blocks-server/connection/blocks.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose').default;
 require('dotenv').config();
 
-const URI = process.env.BLOCKSDB || 'mongodb://localhost:27017/blocks'
+const URI = process.env.MONGOURI || 'mongodb://localhost:27017/blocks'
 
 const conn = mongoose.createConnection(URI, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
     if (err) {
