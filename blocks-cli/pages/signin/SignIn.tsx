272cd3469bacd44c53000632ef4c155a8cf7e04b
jns-w <jns.w@icloud.com>
2024-03-07 22:39:18 +0800

    prod troubleshoot

--- blocks-cli/pages/signin/SignIn.tsx
+++ blocks-cli/pages/signin/SignIn.tsx
@@ -12,7 +12,6 @@ import {useRouter} from "next/router";
 import {useEventListener} from "usehooks-ts";
 import {useAtom} from "jotai";
 import {authTokenAtom, userAtom} from "@/atoms";
-import { unstable_noStore as noStore } from 'next/cache'
 
 const SignIn = () => {
   const [inputs, setInputs] = useState({email: "", password: ""})
@@ -25,9 +24,6 @@ const SignIn = () => {
   const router = useRouter()
   const passwordRef: React.Ref<any> = useRef()
   const emailRef: React.Ref<any> = useRef()
-
-  noStore()
-  console.log('noStore', process.env.NEXT_PUBLIC_SECRET)
 
   const signIn = async () => {
     try {
